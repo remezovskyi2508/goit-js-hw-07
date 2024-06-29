262d494983866b5469aced7067768178fd7673ac
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2024-06-29 10:31:15 +0300

    finish task-2 [js]

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,6 @@
+const gallery = document.querySelector(".gallery");
+
+
 const images = [
   {
     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
@@ -22,5 +25,13 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+
+images.forEach((item) => { // Перебираємо item в масиві images
+  gallery.insertAdjacentHTML(
+    "beforeend", // вкладаємо в середеину ul
+    `<li><img src="${item.url}" alt="${item.alt}"></li>`
+  );
+});
