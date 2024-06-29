783309e72a849ee987ce9c4ae7a9caedd8c8bb21
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2024-06-29 11:30:37 +0300

    finish task-3 [js]

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("#name-input");
+const h1Element = document.querySelector("#name-output");
+
+
+input.addEventListener("input", event => {
+    const trimValue = event.currentTarget.value.trim();
+    if (trimValue === "") {
+       h1Element.textContent = "Anonymous";
+    } else {
+        h1Element.textContent = trimValue;
+    }
+});
