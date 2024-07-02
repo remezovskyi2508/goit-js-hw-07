a52dcc9b4e317602aef0884424bc59db8040ec66
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2024-07-02 23:14:37 +0300

    finish task-5[js]

--- js/task-5.js
+++ js/task-5.js
@@ -1,16 +1,32 @@
 const changeColor = document.querySelector("body");
-const changeButton = document.querySelector("button['change-color']");
-const textColor = document.querySelector("color");
+const changeButton = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+const widget = document.querySelector(".widget");
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return `rgb(${[r, g, b].join(", ")})`;
 }
 
-changeColor.style.backgroundColor = getRandomHexColor; 
+changeButton.addEventListener("click", () => {
+  let rgbColor = `${getRandomHexColor()}`;
+  changeColor.style.backgroundColor = rgbColor;
+  textColor.textContent = rgbColor;
+});
 
 
 
-
-
+widget.style.textAlign = "center";
+widget.style.fontFamily = "Montserrat";
+widget.style.fontWeight = "400";
+changeButton.style.fontWeight = "500";
+changeButton.style.border = "none";
+widget.style.fontSize = "16px";
+widget.style.lineHeight = "24px"; 
+widget.style.letter = "4%";
+changeButton.style.backgroundColor = "#4E75FF";
+changeButton.style.color = "white";
+changeButton.style.padding = "8px 16px";
+changeButton.style.borderRadius = "8px";
