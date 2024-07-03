b7d2445dc5872314af5134ecd6a0d991efcf1fb3
Anton Remezovskyi <antonremezovskyi@gmail.com>
2024-07-03 16:54:10 +0300

    task-4 [js] + style

--- js/task-4.js
+++ js/task-4.js
@@ -2,6 +2,8 @@ const form = document.querySelector(".login-form");
 
 const btn = document.querySelector(".button[type='submit']");
 
+const btnStyle = document.querySelector("button");
+
 form.addEventListener("submit", event => {
     event.preventDefault();
     
@@ -25,3 +27,34 @@ form.addEventListener("submit", event => {
     form.reset();
 
 });
+
+//Питання чи треба було прописувати стилі в нашому ДЗ?
+
+
+form.style.display = "block";
+form.style.margin = "0 auto";
+form.style.padding = "24px";
+form.style.border = "none";
+form.style.borderRadius = "8px";
+form.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.1)";
+form.style.width = "100%";
+form.style.maxWidth = "300px";
+form.style.fontFamily = "Montserrat";
+form.style.fontWeight = "400";
+
+btnStyle.style.display = "block";
+btnStyle.style.marginTop = "16px";
+btnStyle.style.heigth = "40px";
+btnStyle.style.width = "86px";
+btnStyle.style.border = "none";
+btnStyle.style.fontSize = "16px";
+btnStyle.style.cursor = "pointer";
+btnStyle.style.fontWeight = "500";
+btnStyle.style.lineHeight = "24px";
+btnStyle.style.letter = "4%";
+btnStyle.style.backgroundColor = "#4E75FF";
+btnStyle.style.color = "white";
+btnStyle.style.padding = "8px 16px";
+btnStyle.style.borderRadius = "8px";
+btnStyle.style.fontWeight = "500";
+
